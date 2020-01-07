21e7082474ade28708d873609755560193686b46
daniel benedik <45199837+danielbenedik@users.noreply.github.com>
2020-01-07 16:49:31 +0200

    Add files via upload

---
+++ forms/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formSkiRentColumbia
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            StartWin runApp = new StartWin();
+            if(runApp.DialogResult != DialogResult.Cancel)
+            {
+                runApp.ShowDialog();
+            }
+
+            if(runApp.DialogResult == DialogResult.Yes)
+            {
+                while(runApp.IsUserWantSaveAgain == DialogResult.Yes && runApp.DialogResult == DialogResult.Cancel)
+                {
+                    runApp.Save_data_to_excel();
+                }
+            }
+        }
+    }
+}
